e9662687392dc9b3110b457f37914ce0f2080a06
PixoulIndex <54551520+PixoulIndex@users.noreply.github.com>
2022-12-05 14:07:09 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+<<<<<<< Updated upstream
 print("Success")
 
 
 a = "test1"
+=======
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+
+>>>>>>> Stashed changes
