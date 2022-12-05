93e7febd65d697c68f0280fdfb8dd07560d77544
RyanRandazzo <ryan.randazzo@student.csulb.edu>
2022-12-05 13:52:12 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
+
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+
 print("Success")
 a = "test1"
+print("\nHello\n")
